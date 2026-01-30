f60fa3368cb7b8f9256ef558bf4b53ccd186c7fd
Arva Lazuardy Tristrian 25 <lazuarva@gmail.com>
2026-01-30 17:10:58 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://efdzffpndsvxhzpugfad.supabase.co'
-const supabaseKey = 'sb_publishable_GNDXjm0AJN4MYhfs3M3OWg_OIAMx5qS'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
